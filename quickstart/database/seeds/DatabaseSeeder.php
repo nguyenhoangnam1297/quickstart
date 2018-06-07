8f8e128b4c76d40ac1b19b9389f55d96fe34f63b
nguyenhoangnam1297 <hoangnamit1297@gmail.com>
2018-06-08 06:45:56 +0700

    Database, model, seeding

--- quickstart/database/seeds/DatabaseSeeder.php
+++ quickstart/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->truncate();
+        App\User::create([       
+        	'name' => 'NguyenHoangNam',
+        	'password' => bcrypt(123456789),
+            'email'=>'hoangnamit1297@gmail.com',
+        ]) ;
     }
 }
